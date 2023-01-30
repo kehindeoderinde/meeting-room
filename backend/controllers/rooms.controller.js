90dcd55b93b0c464beba4514d5e981badb973890
kehindeoderinde <kehinde.oderinde@deutsche-boerse.com>
2023-01-30 12:12:03 +0000

    format response and schema for rooms

--- backend/controllers/rooms.controller.js
+++ backend/controllers/rooms.controller.js
@@ -1,7 +1,51 @@
 const { Room } = require("../models/rooms.model")
 
-exports.getRooms = (req, res) => {
-    res.send('All rooms')
+exports.getRooms = async (req, res) => {
+     try{
+        let query =  Room.find({})
+
+        //FILTERING
+        if(req.query){
+            const queriesToOmit = ['page', 'page_size', 'total', 'sort', 'order_by']
+            const queryObj = {...req.query}
+            queriesToOmit.forEach(q => {if(queryObj.hasOwnProperty(q)) delete queryObj[q]})
+            query = query.find(queryObj)
+        }
+
+        //SELECT PROPERTIES TO OMIT FROM  RESPONSE
+        query = query.select('-__v')
+
+        //PAGINATION
+        const skip = req.query.page ?  (req.query.page - 1) * 15 : 0;
+        const pageSize = req.query.page_size || 15
+
+        if(req.query.page){
+            const total = query.countDocuments()
+
+            if(skip >= total) throw new Error('Page not found')
+        }
+
+        query = query.skip(skip).limit(pageSize)
+
+        const rooms = await query
+
+        res.status(200).json({
+            meta: {
+                paging: {
+                    page: req.query.page || 1,
+                    page_size: pageSize,
+                    total: rooms.length
+                }
+            },
+            data: rooms
+        })
+
+     } catch(e) {
+        res.status(400).json({
+            status: "failed",
+            error: err
+        })
+     }
 }
 
 exports.createRoom = (req, res) => {
--- backend/models/rooms.model.js
+++ backend/models/rooms.model.js
@@ -12,12 +12,23 @@ const roomSchema = new mongoose.Schema({
     },
     capacity: {
         type: Number,
+        min: 4,
+        max: 10,
         required: [true, 'A room should have a defined capacity']
     },
     floor: {
         type: Number,
         required: [true, 'A room must be allocated to a certain floor']
-    }
+    },
+    created_at: {
+        type: Date,
+        default: Date.now(),
+        select: false
+    },
+    deleted_at: {
+        type: Date,
+        select: false
+    },
 })
 
 exports.Room = mongoose.model('Room', roomSchema)
